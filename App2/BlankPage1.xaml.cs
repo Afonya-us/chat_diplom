ac5d6b3b52f028f593dd3c26d4a05bc2ae0cd95d
Afonya-us <121076903+Afonya-us@users.noreply.github.com>
2023-05-14 20:42:11 +0500

    rtedt

---
+++ App2/BlankPage1.xaml.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data.SqlClient;
+using Windows.UI;
+using System.Globalization;
+using Windows.UI.Xaml.Shapes;
+using Microsoft.Toolkit.Uwp.UI.Media;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Media.Animation;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Xml.Linq;
+using Windows.UI.Xaml.Documents;
+using System.Drawing;
+using System.Collections;
+using System.Timers;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
+using System.Runtime.Serialization.Formatters.Binary;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App2
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public string connection_string;
+        public object arr;
+        public int user;
+        public string conn;
+        public string user_flist;
+        public Boolean dg_op = false;
+        public int msg_counter = 0;
+        public byte[] us_img_data;
+        public Boolean img_bool;
+
+        public int users_count;
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            arr = e.Parameter;
+            string[] splited = arr.ToString().Split("_");
+            conn = splited[1].ToString();
+            user = Convert.ToInt32(splited[0]);
+            connection_string = "Server=" + conn + "; Database = Chat; user=admin; password=admin; encrypt = false;";
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                connection.Open();
+                SqlCommand comm = new SqlCommand("SELECT COUNT(*) FROM dbo.user_list", connection);
+                users_count = (Int32)comm.ExecuteScalar();
+                SqlDataAdapter adapter = new SqlDataAdapter("select * from dbo.user_list", connection);
+                // Создаем объект DataSet
+                DataSet ds = new DataSet();
+                // Заполняем Dataset
+                adapter.Fill(ds);
+                DataTable dt = ds.Tables[0];
+
+                for (int i = 0; i < (int)users_count; i++)
+                {
+
+                    Border bord = new Border();
+                    bord.Height = 80;
+                    bord.Margin = new Thickness(10, 10, 20, 10);
+                    bord.Background = new SolidColorBrush(Colors.White);
+                    bord.BorderThickness = new Thickness(1);
+                    bord.BorderBrush = new SolidColorBrush(Colors.Black);
+                    bord.CornerRadius = new CornerRadius(15);
+                    bord.Tapped += new TappedEventHandler(bord_tap);
+                    bord.Name = dt.Rows[i][0].ToString();
+                    msgs.Children.Add(bord);
+
+                    TextBlock tb_user = new TextBlock();
+                    tb_user.Text = dt.Rows[i][6].ToString();
+                    tb_user.VerticalAlignment = VerticalAlignment.Center;
+                    tb_user.HorizontalAlignment = HorizontalAlignment.Center;
+                    bord.Child = tb_user;
+                }
+            }
+        }
+
+        public async void load_image()
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                () => { 
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+
+                SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM dbo.user_list where user_id='{user}'", connection);
+                // Создаем объект DataSet
+                DataSet dataset = new DataSet();
+                // Заполняем Dataset
+                adapter.Fill(dataset);
+                DataTable dt = dataset.Tables[0];
+                user_name.Text = dt.Rows[0][6].ToString();
+                var imag = dt.Rows[0][7];
+                        BinaryFormatter bf = new BinaryFormatter();
+                        using (var ms = new MemoryStream())
+                        {
+                            bf.Serialize(ms, imag);
+                            ms.ToArray();
+                        
+                        if (ms.Length > 102)
+                {
+                    get_img();
+                }
+                else
+                {
+                    user_image.Background = new SolidColorBrush(Colors.Azure);
+                }
+                        }
+                    } });
+        }
+
+
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+            load_image();
+            
+        }
+
+        public async void get_img()
+        {           
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command = new SqlCommand("select user_img from dbo.user_list where user_id=@user", connection);
+                command.Parameters.AddWithValue("@user", user);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        us_img_data = (byte[])reader.GetValue(0);
+                    }
+
+                    using (var stream = new InMemoryRandomAccessStream())
+                    {
+                        using (var writer = new DataWriter(stream.GetOutputStreamAt(0)))
+                        {
+                            writer.WriteBytes(us_img_data);
+                            await writer.StoreAsync();
+                        }
+
+                        var bitmap = new BitmapImage();
+                        await bitmap.SetSourceAsync(stream);
+
+                        var brush = new ImageBrush { ImageSource = bitmap };
+                        await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                        () =>
+                        {
+                            user_image.Background = brush;
+
+                        });
+                    }
+                }
+            }
+        }
+
+       
+
+
+
+
+
+        public void timer_Elapsed(object state)
+        {
+            int lst_chk = 0;
+            if (dg_op == true)
+            {
+
+                using (SqlConnection connection = new SqlConnection(connection_string))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand($"select count(*) from dbo.msg_list where msg_sender='{user}' or msg_getter='{user}'", connection);
+                    lst_chk = (Int32)cmd.ExecuteScalar();
+                    if (lst_chk > msg_counter)
+                    {
+                        show_msgs();
+                    }
+                }
+
+            }
+            else
+            {
+                Thread.Sleep(-1);
+            }
+        }
+
+        public async void show_msgs()
+        {
+
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand($"select count(*) from dbo.msg_list where msg_sender='{user}' or msg_getter='{user}'", connection);
+                msg_counter = (Int32)cmd.ExecuteScalar();
+            }
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                chat.Children.Clear();
+                using (SqlConnection connection = new SqlConnection(connection_string))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter($"select * from dbo.msg_list where msg_sender='{user}' and msg_getter='{user_flist}' or msg_getter='{user}' and msg_sender='{user_flist}' order by msg_sent asc", connection);
+                    // Создаем объект DataSet
+                    DataSet ds = new DataSet();
+                    // Заполняем Dataset
+                    adapter.Fill(ds);
+                    DataTable dt = ds.Tables[0];
+                    int msg_count = dt.Rows.Count;
+
+                    for (int i = 0; i < msg_count; i++)
+                    {
+                        if (Convert.ToInt32(dt.Rows[i][1]) == user)
+                        {
+                            Border msg = new Border();
+                            msg.Margin = new Thickness(10, 10, 20, 10);
+                            msg.Background = new SolidColorBrush(Colors.LightGray);
+                            msg.MaxWidth = 400;
+                            msg.CornerRadius = new CornerRadius(15);
+                            msg.HorizontalAlignment = HorizontalAlignment.Right;
+                            chat.Children.Add(msg);
+
+                            TextBlock tb_user = new TextBlock();
+                            string date = Convert.ToDateTime(dt.Rows[i][6]).ToString("dd.MM.yyyy HH:mm");
+                            tb_user.Text = $"{dt.Rows[i][3].ToString()}\n\n {date}";
+                            tb_user.VerticalAlignment = VerticalAlignment.Center;
+                            tb_user.Margin = new Thickness(10, 10, 10, 10);
+                            tb_user.TextWrapping = TextWrapping.Wrap;
+                            tb_user.MaxWidth = 400;
+                            tb_user.HorizontalAlignment = HorizontalAlignment.Center;
+                            msg.Child = tb_user;
+
+
+                        }
+                        else
+                        {
+                            Border msg = new Border();
+                            msg.Margin = new Thickness(10, 10, 20, 10);
+                            msg.Background = new SolidColorBrush(Colors.LightBlue);
+                            msg.CornerRadius = new CornerRadius(15);
+                            msg.MaxWidth = 400;
+                            msg.HorizontalAlignment = HorizontalAlignment.Left;
+                            chat.Children.Add(msg);
+
+                            TextBlock tb_user = new TextBlock();
+                            string date = Convert.ToDateTime(dt.Rows[i][6]).ToString("dd.MM.yyyy HH:mm");
+                            tb_user.Text = $"{dt.Rows[i][3]}\n\n {date}";
+                            tb_user.Margin = new Thickness(10, 10, 10, 10);
+                            tb_user.VerticalAlignment = VerticalAlignment.Center;
+                            tb_user.TextWrapping = TextWrapping.Wrap;
+                            tb_user.MaxWidth = 400;
+                            tb_user.HorizontalAlignment = HorizontalAlignment.Center;
+                            msg.Child = tb_user;
+                        }
+                    }
+                }
+            });
+            }
+    
+        
+        public void bord_tap(object sender, TappedRoutedEventArgs e)
+        {
+            user_flist = ((FrameworkElement)sender).Name;
+            dg_op = true;
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand($"select user_nickname from dbo.user_list where user_id={user_flist}", connection);
+                nick_chat.Text = cmd.ExecuteScalar().ToString();
+            }
+            var myTimer = new System.Threading.Timer(timer_Elapsed, null, 0, 1000);
+            chat.Children.Clear();
+            show_msgs();
+        }
+
+        
+
+        private void send_btn_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter($"select * from dbo.msg_list order by msg_upd asc", connection);
+                // Создаем объект DataSet
+                DataSet ds = new DataSet();
+                // Заполняем Dataset
+                adapter.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                string message = msg_text.Text;
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand($"insert into dbo.msg_list(msg_sender,msg_getter,msg_text,msg_file,msg_sent) values ('{user}', '{user_flist}','{message}',null,getdate())",connection);
+                cmd.ExecuteNonQuery();
+
+                Border msg = new Border();
+                msg.Margin = new Thickness(10, 10, 20, 10);
+                msg.Background = new SolidColorBrush(Colors.Gray);
+                msg.MaxWidth = 400;
+                msg.CornerRadius = new CornerRadius(15);
+                msg.HorizontalAlignment = HorizontalAlignment.Right;
+                chat.Children.Add(msg);
+
+                TextBlock tb_user = new TextBlock();
+                tb_user.Text = $"{msg_text.Text}\n\n {DateTime.Now.ToString("dd.MM.yyyy HH:mm")}";
+                tb_user.Margin = new Thickness(10, 20, 20, 10);
+                tb_user.VerticalAlignment = VerticalAlignment.Center;
+                tb_user.Margin = new Thickness(10, 10, 10, 10);
+                tb_user.TextWrapping = TextWrapping.Wrap;
+                tb_user.MaxWidth = 400;
+                tb_user.HorizontalAlignment = HorizontalAlignment.Center;
+                msg.Child = tb_user;
+
+                
+
+                msg_text.Text = "";
+                
+            }
+        }
+
+        
+
+        private void close_dial_Click(object sender, RoutedEventArgs e)
+        {
+            dg_op = false;
+            chat.Children.Clear();
+           
+                nick_chat.Text = string.Empty;
+            
+        }
+
+        
+
+       
+    }
+}
+    
---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data.SqlClient;
+using Windows.UI;
+using System.Globalization;
+using Windows.UI.Xaml.Shapes;
+using Microsoft.Toolkit.Uwp.UI.Media;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Media.Animation;
+using Microsoft.Identity.Client;
+using System.Text.RegularExpressions;
+
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace App2
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public bool con_status = false;
+        public string conn;
+        public int user;
+        public string data="";
+
+        public MainPage()
+        {
+            this.InitializeComponent();          
+            round_st.Fill = new SolidColorBrush(Colors.Gray);
+            string ServerInfo = string.Empty;
+
+
+
+
+
+            /*round.Visibility = Visibility.Visible;
+
+            DoubleAnimation buttonAnimation = new DoubleAnimation();
+            buttonAnimation.From = round.ActualWidth;
+            buttonAnimation.To = 150;
+            buttonAnimation.Duration = TimeSpan.FromSeconds(3);
+            buttonAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            round.be*/
+
+
+            //Получение доступных SQL серверов.
+            DataTable dt = Microsoft.Data.Sql.SqlDataSourceEnumerator.Instance.GetDataSources();
+            foreach (DataRow dr in dt.Rows)
+            {
+                //Вывод найденной информации о серверах
+                //в элемент управления СomboBox.
+                combobox1.Items.Add(string.Concat(dr["ServerName"], @"\", dr["InstanceName"]));
+
+                //Добавление пустой строки после получения
+                //информации о сервере
+                foreach (DataColumn col in dt.Columns)
+                {
+                    ServerInfo += String.Format("{0,-15}: {1}", col.ColumnName, dr[col]) + Environment.NewLine;
+                }
+
+                //Добавление пустой строки после получения
+                //информации о сервере.
+                ServerInfo += Environment.NewLine;
+            }
+        }
+
+
+
+        private void pass_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+
+
+
+        
+
+       
+        private void combobox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {           
+            textblock.Text = combobox1.SelectedItem.ToString();
+            conn=combobox1.SelectedItem.ToString();
+            using (SqlConnection connection = new SqlConnection($"Server='{conn}'; Database = Chat; user=admin; password=admin; encrypt = false;"))
+            {
+                try
+                {
+                    login.IsEnabled = false;
+                    pass.IsEnabled = false;
+                    button1.IsEnabled = false;
+                    
+
+                    connection.Open();
+                }
+                catch (Exception ex) {
+                    round_st.Fill = new SolidColorBrush(Colors.Orange);
+                    textblock.Text = $"Непредвиденная ошибка: {ex}";
+                    con_status = false;
+                    login.IsEnabled = true;
+                    pass.IsEnabled = true;
+                    button1.IsEnabled = true;
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        textblock.Text = "не робит";
+                        round_st.Fill = new SolidColorBrush(Colors.Red);
+                        con_status = false;
+                        login.IsEnabled = true;
+                        pass.IsEnabled = true;
+                        button1.IsEnabled = true;
+                    }
+                    else
+                    {
+                        textblock.Text = "робит";
+                        round_st.Fill = new SolidColorBrush(Colors.Green);
+                        con_status = true;
+                        login.IsEnabled = true;
+                        pass.IsEnabled = true;
+                        button1.IsEnabled = true;
+                    }
+                }
+            }
+
+        }
+
+        
+
+           
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (con_status == false)
+            {
+                var dialog = new MessageDialog("Вы не выбрали сервер");
+                dialog.Commands.Add(new UICommand("Ok", null));
+                dialog.DefaultCommandIndex = 0;
+                dialog.CancelCommandIndex = 1;
+                await dialog.ShowAsync();
+            }
+            else
+            {
+
+                using (SqlConnection connection = new SqlConnection($"Server='{conn}'; Database = Chat; user=admin; password=admin; encrypt = false;"))
+                {
+                    string sql = "select * from dbo.User_list";
+                    SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                    // Создаем объект DataSet
+                    DataSet ds = new DataSet();
+                    // Заполняем Dataset
+                    adapter.Fill(ds);
+                    DataTable dt = ds.Tables[0];
+                    //Поиск строки по id
+                    DataRow[] foundRows;              
+                    foundRows = dt.Select($"user_login='{login.Text}'");
+                    if (foundRows.Count() >= 1 && pass.Password != string.Empty && login.Text!=string.Empty)
+                    {
+                        int i = 0;
+                        while (i < foundRows.Count())
+                        {
+                            if (foundRows[i][2].ToString() == pass.Password)
+                            {
+                                
+                                user =Convert.ToInt32(foundRows[i][0]);
+
+                                data = $"{user}_{conn}";
+                                Frame.Navigate(typeof(BlankPage1), data);
+                                
+                                this.Visibility = Visibility.Collapsed;
+                            }
+                            else
+                            {
+                                textblock.Text = "Не верный пароль";
+                                textblock.Foreground = new SolidColorBrush(Colors.Red);
+                                pass.Password = string.Empty;
+                            }
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        if (foundRows.Count() == 0)
+                        {
+                            textblock.Text = "Не верный или пустой логин";
+                            textblock.Foreground = new SolidColorBrush(Colors.Red);
+                            login.Text = string.Empty;
+                            pass.Password = string.Empty;
+                        }
+
+                    }
+                }
+            }
+
+        }
+        
+    }
+}
